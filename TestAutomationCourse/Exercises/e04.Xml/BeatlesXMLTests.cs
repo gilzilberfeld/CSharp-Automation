05c4a7c8057bba2423957321887d00a18f44c30a
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-20 16:53:27 +0300

    Added xml exercise and solution

---
+++ TestAutomationCourse/Exercises/e04.Xml/BeatlesXMLTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Xml;
+
+namespace TestAutomationCourse.Exercises.e04.Xml
+{
+    internal class BeatlesXMLTests
+    {
+        private XmlDocument doc;
+
+        [SetUp]
+        public void Setup()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(".//Exercises//e04.Xml//Beatles.xml");
+        }
+
+        [Test]
+        public void There_are_four_artists()
+        {
+        }
+
+        [Test]
+        public void Two_are_dead_and_two_are_alive()
+        {
+
+        }
+
+        [Test]
+        public void Ringo_plays_drums()
+        {
+
+        }
+
+    }
+}
---
+++ TestAutomationCourse/Solutions/e04.XML/BeatlesXMLTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System.Xml;
+
+namespace TestAutomationCourse.Solutions.e04.Xml
+{
+    internal class BeatlesXMLTests
+    {
+        private XmlDocument xmlDoc;
+        private XmlElement root_element;
+
+        [SetUp]
+        public void Setup()
+        {
+            xmlDoc = new XmlDocument();
+            xmlDoc.Load(".//Exercises//e04.Xml//Beatles.xml");
+            root_element = xmlDoc.DocumentElement; 
+        }
+
+        [Test]
+        public void There_are_four_artists()
+        {
+            var artist_list = root_element.GetElementsByTagName("Artist");
+            Assert.That(artist_list.Count, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Two_are_dead_and_two_are_alive()
+        {
+            var artist_list = root_element.GetElementsByTagName("Artist");
+            int countAlive = 0;
+            int countDead = 0;
+
+            foreach (XmlElement artist in artist_list)
+            {
+                string isAliveText = artist.GetElementsByTagName("IsAlive").Item(0).InnerText;
+                if (isAliveText.Equals("Yes"))
+                    countAlive++;
+                if (isAliveText.Equals("No"))
+                    countDead++;
+            }
+            Assert.That(countAlive, Is.EqualTo( 2) );
+            Assert.That(countDead, Is.EqualTo( 2) );
+
+        }
+
+        [Test]
+        public void Ringo_plays_drums()
+        {
+            bool ringo_plays_drums = false;
+            var artist_list = root_element.GetElementsByTagName("Artist");
+            foreach (XmlElement artist in artist_list)
+            {
+                string name = artist.Attributes["name"].Value;
+                if (name.Equals("Ringo Starr"))
+                {
+                    var plays_element = (XmlElement) artist.GetElementsByTagName("Plays").Item(0);
+                    if (plays_element.InnerText.Equals("Drums"))
+                        ringo_plays_drums = true;
+                    break;
+                }
+            }
+            Assert.That(ringo_plays_drums, Is.True);
+
+        }
+
+    }
+}

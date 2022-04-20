fe61daf5e2b39844b9d0b9bad9e7af24c050cdf1
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-20 16:18:56 +0300

    Added XML example

---
+++ TestAutomationCourse/Demos/d02.XML/XMLExamples.cs
@@ -0,0 +1,29 @@
+﻿namespace TestAutomationCourse.Demos.d02.XML
+{
+    internal class XMLExamples
+    {
+        //    <Person>
+        //          <FirstName>Joe</FirstSmith>
+        //          <SurName>Smith</SurName>
+        //          <Children>
+        //              <Child name="Jane"/>
+        //              <Child name="Jim"/>
+        //              <Child name="JJ"/>
+        //          </Children>
+        //      </Person>
+
+        public static string GetPersonXML()
+        {
+            return "<Person>\n" +
+                    "  <FirstName>Joe</FirstName>\n" +
+                    "  <SurName>Smith</SurName>\n" +
+                    "  <Children>\n" +
+                    "    <Child name=\"Jane\"/>\n" +
+                    "    <Child name=\"Jim\"/>\n" +
+                    "    <Child name=\"JJ\"/>\n" +
+                    "  </Children>  \n" +
+                    "</Person>";
+        }
+    }
+}
+

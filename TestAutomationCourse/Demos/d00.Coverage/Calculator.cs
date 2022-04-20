44a1836a8f128a90e1e5add5423e8c41e4b4b79e
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-20 12:34:29 +0300

    d00 coverage

---
+++ TestAutomationCourse/Demos/d00.Coverage/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAutomationCourse.Demos.d00.Coverage
+{
+    public class Calculator
+    {
+        public int add(int i, int j)
+        { 
+            return i + j; 
+        }
+    }
+}
---
+++ TestAutomationCourse/Demos/d00.Coverage/CreditCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestAutomationCourse.Demos.d00.Coverage
+{
+    internal class CreditCard
+    {
+		private DateTime expirationDate;
+		public CreditCard(DateTime expirationDate)
+		{
+			this.expirationDate = expirationDate;
+		}
+
+		public bool IsExpired()
+		{
+			DateTime localExpDate = expirationDate.ToLocalTime();
+			DateTime today = DateTime.Today.ToLocalTime();
+			if (DateTime.Compare(localExpDate, today) > 0)
+				return true;
+			else
+				return false;
+		}
+	}
+}

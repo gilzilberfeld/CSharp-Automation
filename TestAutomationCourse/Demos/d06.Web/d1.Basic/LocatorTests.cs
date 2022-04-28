b1ebb85d0c1ec7e4483cce8c48b3205ef137e13e
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-28 16:58:52 +0300

    more tests

--- TestAutomationCourse/Demos/d06.Web/d1.Basic/LocatorTests.cs
+++ TestAutomationCourse/Demos/d06.Web/d1.Basic/LocatorTests.cs
@@ -27,7 +27,32 @@ namespace TestAutomationCourse.Demos.d06.Web.d1.Basic
             Assert.That(driver.Title, Does.Contain("automation"));
         }
 
-        // relative
+        
+        [Test]
+        public void contains_partial_text()
+        {
+            driver.Url = "http://www.duckduckgo.com";
+            IWebElement tracker_blocking_label = driver.FindElement(By.XPath(
+                ".//span[contains(text(), 'locki')]"));
+            Assert.That(tracker_blocking_label.Text, Is.EqualTo("Tracker Blocking"));
+
+            IWebElement site_encryption_label = driver.FindElement(By.XPath(
+                ".//*[starts-with(., 'sit')]"));
+            Assert.That(site_encryption_label.Text, Is.EqualTo("Site Encryption"));
+        }
+
+        [Test]
+        public void class_and_relative_xpath()
+        {
+            driver.Url = "http://www.duckduckgo.com";
+            IWebElement div_element = driver.FindElement(By.ClassName("search-wrap--home"));
+            IWebElement search_input = div_element.FindElement(By.XPath(
+                ".//input"));
+            search_input.SendKeys("automation");
+            search_input.Submit();
+            Assert.That(driver.Title, Does.Contain("automation"));
+        }
+
 
         [Test]
         public void click_an_image()

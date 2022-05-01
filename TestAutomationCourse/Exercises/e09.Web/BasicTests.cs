5095807fe9902fd742168adf0510aff3dae3fde5
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-05-01 17:53:22 +0300

    More tests

--- TestAutomationCourse/Exercises/e09.Web/BasicTests.cs
+++ TestAutomationCourse/Exercises/e09.Web/BasicTests.cs
@@ -1,7 +1,8 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace TestAutomationCourse.Exercises.e09.Web
 {
@@ -11,5 +12,35 @@ namespace TestAutomationCourse.Exercises.e09.Web
         // click the "about" link, which is not a link, on the google page
 
         // find the name of the google image
+        IWebDriver driver;
+
+        [SetUp]
+        public void start_browser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void submitting_data_and_verifying()
+        {
+            driver.Url = "https://demo.guru99.com/test/newtours/register.php";
+            IWebElement name_text = driver.FindElement(By.Name("firstName"));
+            name_text.SendKeys("Gil");
+            IWebElement submit_button = driver.FindElement(By.Name("submit"));
+            submit_button.Click();
+            var paragraphs = driver.FindElements(By.TagName("p"));
+            var p_containing_dear_gil = paragraphs.FirstOrDefault(p => p.Text.Contains("Dear Gil"));
+            Assert.That(p_containing_dear_gil, Is.Not.Null);
+            var p_containing_note = paragraphs.FirstOrDefault(p => p.Text.Contains("Note"));
+            Assert.That(p_containing_note.Text, Does.Not.Contain("Gil"));
+        }
+
+        [TearDown]
+        public void close_browser()
+        {
+            driver.Close();
+        }
+
+
     }
 }
